21aee2d5b64d5e65e21a1f3b1eaff0217dd638af
Rachel Sakles <rachel.sakles@yahoo.com>
2020-08-08 22:28:07 -0700

    changes

--- script.js
+++ script.js
@@ -1,40 +1,107 @@
 //create variables 
-var startQuizBtn = document.querySelector("#start-quiz");
-var startPrompt = document.querySelector("#start-prompt");
-var questionPrompt = document.querySelector("#question-prompt");
-
-// List of question and options 
-var questions = [
-    {
-        question: "What is the average life span of a cat?",
-        options: ["12-18 years", "9-15 years", "20-25 years", "8-14 years"],
-        answer: "12-18 years"
-    },
-
-]; 
-var questionIndex = 0;
-
+var startQuizBtn = document.getElementById('start-quiz')
+var questionElement = document.getElementById('question')
+var startPrompt = document.getElementById('start-prompt')
+var questionPrompt = document.getElementById('question-prompt')
+var questionOptions = document.getElementById('question-options')
+var questionIndex = 0
+var score = 0
 
 
 // When start quiz button is clicked,
-startQuizBtn.addEventListener("click", function (e) { 
-// Hide start prompt 
-startPrompt.getElementsByClassName.display= "none"; 
-// Show question prompt 
-questionPrompt.getElementsByClassName.display = "block"; 
-// Show questions prompt with the first question 
-renderQuestion();   
+startQuizBtn.addEventListener('click', function (e) {
+    // Hide start prompt
+    startPrompt.style.display = "none";
 
-})  
+    // Show question prompt   
+    questionPrompt.style.display = "block";
 
-function renderQuestion() { 
-    varquestion = question[questionIndex]; 
-    questionOptions = question.options;
+    // populate questions prompt with first question 
+    renderQuestion();
+
+})
+
+
+
+
+//Functions 
+function startQuiz() {
+    console.log('Started')
+    startQuizBtn.classList.add('hide')
+    questionPrompt.classList.remove('hide')
+    setNextQuestion()
+
+}
+
+function setNextQuestion() {
+    showQuestion[questionIndex]
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+
+
+
+
+
+// List of question, options, and correct answer 
+var questions = [
+    {
+        text: "What is the average life span of a cat?",
+        options: ["12-18 years", "9-15 years", "20-25 years", "8-14 years"],
+        answer: "12-18 years"
+    },
+    {
+        text: "On average how many hours do cats sleep in a day?",
+        options: ["8", "10", "15", "20"],
+        answer: "15"
+    },
+    {
+        text: "What is the normal body temperature of a cat?",
+        options: ["98°F", "102°F", "94°F", "106°F"],
+        answer: "102°F"
+    },
+    {
+        text: "What is the proper term for a group of Kittens?",
+        options: ["kindle", "kettle", "kine", "kaboodle"],
+        answer: "kindle"
+    },
+    {
+        text: "What is the largest breed of cat?",
+        options: ["Savannah", "British Shorthair", "Ragdoll", "Maine Coon"],
+        answer: "Maine Coon"
+    },
+];
+
+
+function renderQuestion() {
+    var question = questions[questionIndex];
+    questionText.textContent = question.text;
+    questionOptions.innerHTML= "";
+
+    // We want to loop through all questions of the array
+    for (var i = 0; i < questions.length; i++) {
+        var btn = document.createElement("button"); 
+        btn.setAttribute("class", "btn btn-danger btn-sm"); 
+        btn.setAttribute("value", question.options[i]);
+        btn.textContent = question.options[i];
+        questionOptions.append(btn);
+    }
 } 
 
+document.body.addEventListener('click', function (e) { 
+    if (!e.target.matches(".question-option")) return; 
+    var value = e.target.value; 
+    if (questions[questionIndex].answer === value) { 
+        console.log("correct");
+    }else { 
+        console.log("wrong")
+    }
 
-// User starts with quiz title and start quiz button 
-// When user hits start quiz button the title and button dissappear 
-// Timer begins and questions pop up 
-// If user answers correctly the next question pops up- alert correct 
-// If user gets a question wrong 5 seconds are subtracted from time and next question pops up-alert wrong
+});
